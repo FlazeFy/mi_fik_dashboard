9e96dae8458aae0fe241f75673185be9393be5db
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2022-11-07 23:57:35 +0700

    some API fixes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -38,7 +38,9 @@ class DashboardController extends Controller
 
     public function getAllContent()
     {
-        $cnt = content::all();
+        $cnt = content::orderBy('created_at', 'DESC')
+            ->orderBy('id', 'DESC')
+            ->paginate(15);
         //Need pagination?
         return response()->json($cnt);
     }
@@ -47,6 +49,16 @@ class DashboardController extends Controller
     {
         $cnt = content::where('id', $id)->get();
         
+        return response()->json($cnt);
+    }
+
+    public function getAllSchedule($date)
+    {
+        // $cnt = content::where(date('content_date_start'), $date)->get();
+        $cnt = DB::table('content')
+            ->whereRaw("date(`content_date_start`) = ?", $date)
+            ->orderBy('content.content_date_start', 'DESC')->get();
+        //Need pagination?
         return response()->json($cnt);
     }
     
@@ -59,10 +71,40 @@ class DashboardController extends Controller
     
     public function getMyArchieve($id_user)
     {
-        $ar = archieve::where('id_user', $id_user)->get();
+        // $ar = archieve::where('id_user', $id_user)->get();
+        // Old select raw
+        // archieve::selectRaw("archieve.id, archieve.archieve_name,  CASE WHEN content.content_type = 'event' THEN COUNT(content.id) ELSE 0 END AS event, CASE WHEN content.content_type = 'task' THEN COUNT(content.id) ELSE 0 END AS task")
+        $ar = archieve::selectRaw("archieve.id, archieve.archieve_name,  CASE WHEN archieve_relation.rel_type = 'content' THEN COUNT(content.id) ELSE 0 END AS event, CASE WHEN archieve_relation.rel_type = 'task' THEN COUNT(content.id) ELSE 0 END AS task")
+            ->join('archieve_relation', 'archieve.id', '=', 'archieve_relation.archieve_id')
+            ->join('content', 'content.id', '=', 'archieve_relation.content_id')
+            // ->join('task', 'task.id', '=', 'archieve_relation.content_id')
+            ->where('archieve.id_user', $id_user)
+            ->groupBy('archieve.id')
+            ->orderBy('archieve.created_at', 'DESC')->get();
         
         return response()->json($ar);
     }
 
-   
+    public function addContent(Request $request, $id_user)
+    {
+        $result = content::create([
+            'id_user' => $id_user,
+            'content_title' => $request->content_title,
+            'content_subtitle' => $request->content_subtitle,
+            'content_desc' => $request->content_desc,
+            'content_attach' => $request->content_attach,
+            'content_tag' => $request->content_tag,
+            'content_loc' => $request->content_loc,
+            'content_date_start' => $request->content_date_start,
+            'content_date_end' => $request->content_date_end,
+            'created_at' => date("Y-m-d h:i"),
+            'updated_at' => date("Y-m-d h:i")
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Content successfully added',
+            'result' => $result,
+        ]);
+    }
 }
