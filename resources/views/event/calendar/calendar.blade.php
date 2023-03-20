c3ac5fedca81b6d722b6b893c9bea32616eb6886
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2023-03-20 23:58:44 +0700

    tidy up css, fix calendar page design

--- resources/views/event/calendar/calendar.blade.php
+++ resources/views/event/calendar/calendar.blade.php
@@ -3,48 +3,6 @@
         width: 100%;
         margin: 40px auto;
     }
-    #calendar a {
-        text-decoration: none !important;
-        color:#414141;
-    }
-    .fc-daygrid-event{
-        background: white !important;
-        padding: 0px 10px;
-        white-space: normal !important;
-        margin-inline: 6px !important;
-        font-weight: 500;
-        border-radius: 6px;
-        border-left: #F78A00 4px solid;
-        box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-        margin-bottom: 8px;
-    }
-    .fc-h-event{
-        border:none;
-        border-radius:6px;
-    }
-    .fc-daygrid-event-dot{
-        border:calc(var(--fc-daygrid-event-dot-width,8px)/ 2) solid var(--fc-event-border-color, #198553);
-    }
-    .fc .fc-daygrid-day.fc-day-today{
-        background: rgba(247, 138, 0, 0.4);
-    }
-    .fc-event-time{
-        display:none;
-    }
-    .fc-event-title{
-        color: #414141 !important;
-        white-space: normal !important;
-        font-weight: 500;
-    }
-    /* .fc-daygrid-day-events{
-        display: flex;
-        flex-direction: column;
-        height: 80px;
-        overflow-y: scroll;
-    } */
-    .fc-daygrid-event:hover{
-        background: #F78A00 !important;
-    }
 </style>
 
 <div id="calendar"></div>
@@ -55,15 +13,15 @@
     var calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'dayGridMonth',
         headerToolbar: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'dayGridMonth'
-            //right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            center: 'prev,next today',
+            left: 'title',
+            //right: 'dayGridMonth'
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
         },
         selectable: true,
         navLinks: true, 
-        //eventLimit: true,
-        //eventLimit: 4,
+        eventLimit: true,
+        dayMaxEvents: 4,
         events: [
             <?php
                 //Initial value
@@ -72,6 +30,78 @@
                 foreach($content as $ct){
                     if($ct->content_date_start){
                         echo "
+                            {
+                                groupId: '".$i."',
+                                title: '".$ct->content_title."',
+                                start: '".$ct->content_date_start."',
+                                end: '".$ct->content_date_end."',
+                                extendedProps: {
+                                    slug_name: '".$ct->slug_name."'
+                                }
+                            },
+                            {
+                                groupId: '".$i."',
+                                title: '".$ct->content_title."',
+                                start: '".$ct->content_date_start."',
+                                end: '".$ct->content_date_end."',
+                                extendedProps: {
+                                    slug_name: '".$ct->slug_name."'
+                                }
+                            },
+                            {
+                                groupId: '".$i."',
+                                title: '".$ct->content_title."',
+                                start: '".$ct->content_date_start."',
+                                end: '".$ct->content_date_end."',
+                                extendedProps: {
+                                    slug_name: '".$ct->slug_name."'
+                                }
+                            },
+                            {
+                                groupId: '".$i."',
+                                title: '".$ct->content_title."',
+                                start: '".$ct->content_date_start."',
+                                end: '".$ct->content_date_end."',
+                                extendedProps: {
+                                    slug_name: '".$ct->slug_name."'
+                                }
+                            },
+                            {
+                                groupId: '".$i."',
+                                title: '".$ct->content_title."',
+                                start: '".$ct->content_date_start."',
+                                end: '".$ct->content_date_end."',
+                                extendedProps: {
+                                    slug_name: '".$ct->slug_name."'
+                                }
+                            },
+                            {
+                                groupId: '".$i."',
+                                title: '".$ct->content_title."',
+                                start: '".$ct->content_date_start."',
+                                end: '".$ct->content_date_end."',
+                                extendedProps: {
+                                    slug_name: '".$ct->slug_name."'
+                                }
+                            },
+                            {
+                                groupId: '".$i."',
+                                title: '".$ct->content_title."',
+                                start: '".$ct->content_date_start."',
+                                end: '".$ct->content_date_end."',
+                                extendedProps: {
+                                    slug_name: '".$ct->slug_name."'
+                                }
+                            },
+                            {
+                                groupId: '".$i."',
+                                title: '".$ct->content_title."',
+                                start: '".$ct->content_date_start."',
+                                end: '".$ct->content_date_end."',
+                                extendedProps: {
+                                    slug_name: '".$ct->slug_name."'
+                                }
+                            },
                             {
                                 groupId: '".$i."',
                                 title: '".$ct->content_title."',
